a2cca20ab5239e1555148eff36711aa96acb1673
Hannah Werman <hannah.werman@gmail.com>
2018-05-04 15:41:31 -0500

    added componentWillReceiveProps to allow you to type in the input field

--- src/ClassModal/index.js
+++ src/ClassModal/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css'
 
-const Modal = (props) => {
-	
-	const cssClass = props.showEdit ? 'Modal-Open' : "Modal-Closed"
+// props are immutable, so you can't type anything into the edit field. we need to change it to a class.
 
-	return (
-		<div className={cssClass}>
-			<input type="text" value={props.editedMovie}/>
-			<button> Edit </button>
-		</div>
-	)
+class Modal extends Component {
+	constructor(props) { // you can pass props to the constructor if you want to access them inside this function
+							// the constructor function only gets called one time in the lifetime of the component
+		super(props);
+		this.state = {
+			inputVal: '' 
+		}
+	}
+
+	handleInput = (e) => {
+		this.setState({inputVal: e.currentTarget.value})
+	}
+
+	componentWillReceiveProps(nextProps) { // another built-in function
+		// this --> lifecycle hook <-- will allow you to set state. this is often used to optimize react apps, if you need extra speed. 
+		// allows you to set the state every time the render function is called.
+		this.setState({
+			inputVal: nextProps.editedMovie
+		})
+	}
+	
+	render() {
+		const cssClass = this.props.showEdit ? 'Modal-Open' : "Modal-Closed"; // need this.props here
+		console.log(this.state, "this.state", this.props, "this.props");
+		return (
+			<div className={cssClass}>
+				<input type="text" value={this.state.inputVal} onChange={this.handleInput}/>
+				<button> Edit </button>
+			</div>
+		)
+	}
+	
 
 }
 
